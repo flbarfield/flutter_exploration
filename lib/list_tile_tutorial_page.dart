ff1af6e757171c7e49f7a2779d8c515ce0594ee9
Fredrick Barfield <barfieldfredrick@gmail.com>
2024-04-13 21:16:32 -0700

    mostly experiemented between IDEs, shortcuts, and some structure. Not much to visually show yet

---
+++ lib/list_tile_tutorial_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ListTileTutorialPage extends StatelessWidget {
+  const ListTileTutorialPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
